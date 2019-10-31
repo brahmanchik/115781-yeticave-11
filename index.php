1987b8ec310452bc896d5e5fc2f5a37f26e8c3de
brahmanchik <brahmanchik@gmail.com>
2019-10-31 13:09:39 +0300

    Третье Исправление в соответствии с рекомендациями наставника

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ $ads = [
         <nav class="user-menu">
 
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-        <? if($is_auth==1): ?>
+        <? if($is_auth == 1): ?>
             <div class="user-menu__logged">
                 <p><?= $user_name; ?></p>
                 <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
